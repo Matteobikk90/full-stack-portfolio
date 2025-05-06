3f3174158ce130fc003a567214d82fdcaa810c74
Matteo Soresini <matteo.soresini@hotmail.it>
2025-05-06 05:17:17 +0200

    test(vitest): setup Vitest + basic App test

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+
+// Include below line for test configuration
+/// <reference types="vitest" />
+
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    include: ['src/**/*.test.{ts,tsx}'],
+    exclude: ['node_modules', 'dist'],
   },
 });
