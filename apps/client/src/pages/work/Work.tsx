a1f125b46f6a45371ce763911eecfe1fdac5d009
Matteo Soresini <matteo.soresini@hotmail.it>
2025-06-05 15:02:57 +0200

    feature (work): small issue fixed on backend refresh token

--- apps/client/src/pages/work/Work.tsx
+++ apps/client/src/pages/work/Work.tsx
@@ -1,8 +1,7 @@
 export const Work = () => {
   return (
-    <main>
+    <main className="flex flex-col justify-between gap-12 p-4 container mx-auto relative z-11">
       <h1 className="text-work">Work</h1>
-      <p>Welcome to the Work page!</p>
     </main>
   );
 };
--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -40,6 +40,7 @@ export const handleRefreshToken = (req: Request, res: Response): void => {
 
   if (!refreshToken) {
     res.status(400).json({ message: 'Missing refresh token' });
+    return;
   }
 
   try {
@@ -68,7 +69,11 @@ export const handleRefreshToken = (req: Request, res: Response): void => {
 };
 
 export const getMe = async (req: Request, res: Response) => {
-  const userId = (req.user as { userId: string }).userId;
+  const userId = (req.user && (req.user as { userId: string }).userId) || null;
+
+  if (!userId) {
+    return res.status(401).json({ message: 'Not authenticated' });
+  }
 
   try {
     const user = await prisma.user.findUnique({
