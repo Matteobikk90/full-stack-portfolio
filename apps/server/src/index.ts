e2defe3a5f10c545d8a57b4b3a1f1791ffd786d8
Matteo Soresini <matteo.soresini@hotmail.it>
2025-06-06 15:34:33 +0200

    fix: bind server to 0.0.0.0 for NGINX

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -64,6 +64,6 @@ io.on('connection', async (socket) => {
   });
 });
 
-httpServer.listen(PORT, () => {
-  console.log(`🚀 Server + WebSocket listening on http://localhost:${PORT}`);
+httpServer.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server + WebSocket listening on http://0.0.0.0:${PORT}`);
 });
