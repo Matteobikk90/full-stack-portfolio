dc0eafb608aa4e87b57780f0c2747e39abc70a5e
Matteo Soresini <matteo.soresini@hotmail.it>
2025-06-22 19:32:30 +0200

    auth improved

--- apps/server/src/routes/user.route.ts
+++ apps/server/src/routes/user.route.ts
@@ -1,19 +1,9 @@
-import { authenticateToken } from '@/auth/auth.middleware';
 import { validateBody } from '@/middleware/validate.middleware';
 import { asyncHandler } from '@/utils/async-handlers';
 import { createUserSchema } from '@/validation/user.schema';
 import { Router } from 'express';
 
 const router = Router();
-
-router.get(
-  '/me',
-  authenticateToken,
-  asyncHandler(async (req, res) => {
-    const user = req.user as { userId: string };
-    res.json({ userId: user.userId });
-  })
-);
 
 router.post(
   '/',
