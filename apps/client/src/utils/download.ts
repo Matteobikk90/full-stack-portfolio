e89c920cdf5083c2cbe4e0272ef0c611140be844
Matteo Soresini <matteo.soresini@hotmail.it>
2025-07-14 23:18:02 +0200

    new cv

--- apps/client/src/utils/download.ts
+++ apps/client/src/utils/download.ts
@@ -1,6 +1,6 @@
 export const handleDownload = () => {
   const link = document.createElement('a');
-  link.href = '/cv2025.pdf';
-  link.download = 'CV-MatteoSoresini.pdf';
+  link.href = '/Matteo_Soresini_CV_2025.pdf';
+  link.download = 'Matteo_Soresini_CV_2025.pdf';
   link.click();
 };
