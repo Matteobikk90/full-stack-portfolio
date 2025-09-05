07d4454562b7d57c4971c38390e48a03e9b605da
Matteo Soresini <matteo.soresini@hotmail.it>
2025-09-05 16:48:16 +0200

    pack work updated

--- apps/client/src/utils/slider.ts
+++ apps/client/src/utils/slider.ts
@@ -2,6 +2,7 @@ import ags from '@/assets/images/ags.png';
 import blendon from '@/assets/images/blendon.png';
 import emissions from '@/assets/images/emissions.png';
 import internet from '@/assets/images/internet.png';
+import pack from '@/assets/images/pack.png';
 import portfolio from '@/assets/images/portfolio.png';
 import social from '@/assets/images/social.png';
 import water from '@/assets/images/water.png';
@@ -10,6 +11,7 @@ import youth from '@/assets/images/youth.png';
 export const imageMap: Record<string, string> = {
   'blendon-barber-shop': blendon,
   'full-stack-portfolio': portfolio,
+  'file-upload': pack,
   'ags-illuminazione': ags,
   'world-emissions-clock': emissions,
   'internet-poverty-index': internet,
